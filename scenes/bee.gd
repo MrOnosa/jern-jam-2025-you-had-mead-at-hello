d0807464ffb92cb32bbc59c09d9f0ae199b4add3
MrOnosa <supersnapple@gmail.com>
2025-09-18 20:19:11 -0500

    Beginning bee brain

--- scenes/bee.gd
+++ scenes/bee.gd
@@ -1,5 +1,8 @@
 extends Area2D
 
+enum Objective { LEAVE_BEE_HIVE, FORAGING, FOUND_FOOD, GET_FOOD, BEELING_IT_BACK_TO_THE_HIVE, ENTER_BEE_HIVE  }
+
+var current_objective := Objective.LEAVE_BEE_HIVE
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,17 +13,24 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
-func bee_navigate_generator() -> void:
-	var subtween_spin = create_tween()
-	subtween_spin.tween_property(self, "rotation_degrees", 45.0, randf_range(0.5, 1.5)).set_trans(Tween.TRANS_SINE ) 
-	subtween_spin.tween_property(self, "rotation_degrees", 0.0, randf_range(0.5, 1.5)).set_trans(Tween.TRANS_SINE )
-	
-	
-	var tween = get_tree().create_tween()
-	tween.tween_property(self, "position", world_clamp(position + Vector2(randi_range(-10, 10), randi_range(-10, 30))), randf_range(0.5, 1.5)).set_trans(Tween.TRANS_SINE )
-	tween.tween_subtween(subtween_spin)
-	tween.tween_property(self, "position", world_clamp(position + Vector2(randi_range(-50, 50), randi_range(-100, 300))), randf_range(0.5, 1.5)).set_trans(Tween.TRANS_SINE )
-	tween.tween_callback(bee_navigate_generator)
+func bee_navigate_generator() -> void:	
+	match current_objective:
+		Objective.LEAVE_BEE_HIVE:
+			var tween = get_tree().create_tween()
+			tween.tween_property(self, "position", world_clamp(position + Vector2(randi_range(-10, 10), randi_range(30, 80))), randf_range(0.5, 1.5)).set_trans(Tween.TRANS_SINE )
+			tween.tween_callback(bee_navigate_generator)
+			current_objective = Objective.FORAGING
+		_:	
+			var subtween_spin = create_tween()
+			subtween_spin.tween_property(self, "rotation_degrees", 45.0, randf_range(0.5, 1.5)).set_trans(Tween.TRANS_SINE ) 
+			subtween_spin.tween_property(self, "rotation_degrees", 0.0, randf_range(0.5, 1.5)).set_trans(Tween.TRANS_SINE )
+			
+			
+			var tween = get_tree().create_tween()
+			tween.tween_property(self, "position", world_clamp(position + Vector2(randi_range(-30, 30), randi_range(-30, 30))), randf_range(0.5, 1.5)).set_trans(Tween.TRANS_SINE )
+			tween.tween_subtween(subtween_spin)
+			tween.tween_property(self, "position", world_clamp(position + Vector2(randi_range(-300, 300), randi_range(-300, 300))), randf_range(0.5, 1.5)).set_trans(Tween.TRANS_SINE )
+			tween.tween_callback(bee_navigate_generator)
 	
 func world_clamp(vector: Vector2) -> Vector2:
 	return Vector2(clamp(vector.x, 0, 13000 * .3), clamp(vector.y, 0, 8000 * .3))
