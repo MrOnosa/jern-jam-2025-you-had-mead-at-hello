9d8183806ab9274c15cc82fe6bd02112702f6d7f
MrOnosa <supersnapple@gmail.com>
2025-09-20 13:51:45 -0500

    Instead of generating a noise texture on the fly, I saved it as a .png file that loads instantly. This is much faster and fixes the lag when starting the game. If we need a new texture, the png will need to be regenerated. The code for that was in world.gd as of this commit.

--- scenes/world.gd
+++ scenes/world.gd
@@ -1,9 +1,12 @@
 extends Node2D
 
 const BEE = preload("uid://ws0llatxgtev")
+#const WORLD_GRASS_NOISE_TEXTURE = preload("uid://c0a00g37gmqql")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+#	var img = WORLD_GRASS_NOISE_TEXTURE.get_image()
+#	img.save_png("user://somefile.png")
 	pass # Replace with function body.
 
 
