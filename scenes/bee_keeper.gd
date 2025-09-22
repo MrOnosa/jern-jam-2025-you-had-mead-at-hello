e407f1a3d7f475508f85efa254ed0857c9ff5056
Josh <53954608+swsjoshua@users.noreply.github.com>
2025-09-21 20:41:00 -0400

    Beekeeper left right flip
    - Spent too long making this happen...

--- scenes/bee_keeper.gd
+++ scenes/bee_keeper.gd
@@ -9,18 +9,26 @@ const SPEED = 300.0
 @export var GoToObj : Node2D #idk why this wasnt working but whatever
 @export var player_rig: PlayerAnimations
 
+@onready var player_animations: PlayerAnimations = %PlayerAnimations
+@onready var honey_high_sprite: Sprite2D = %HoneyHighSprite
+
+
 var target_position : Vector2
 var click_position : Vector2
 var holding := Holding.NOTHING
 var anim_state := Action.IDLE
-var player_facing_right := true
+var player_facing_right : bool = true
+var direction_facing
+var player_base_scale: Vector2
 
 func _ready():
 	GoToObj = get_node("../GoingTowardsThisPoint")
 	click_position = position
+	player_base_scale = scale
+	direction_facing = player_base_scale.x
 
 func _process(_delta: float) -> void:
-	%HoneyHighSprite.visible = holding == Holding.HONEY_HIGH
+	honey_high_sprite.visible = holding == Holding.HONEY_HIGH
 	
 	if holding:
 		if anim_state == Action.WALKING:
@@ -33,10 +41,6 @@ func _process(_delta: float) -> void:
 		elif anim_state == Action.IDLE:
 			player_rig.animations.animation = "idle"
 		
-	if player_facing_right:
-		$PlayerAnimations.scale.x = 1
-	else:
-		$PlayerAnimations.scale.x = -1
 
 func _physics_process(_delta: float) -> void:
 	if position.distance_to(click_position) > 3:
@@ -53,7 +57,13 @@ func _unhandled_input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("left_click"):
 		click_position = get_global_mouse_position()
 		GoToObj.global_position = click_position
-		player_facing_right = GoToObj.global_position.x > position.x
+
+		if GoToObj.global_position.x > position.x && scale != Vector2.ONE:
+			scale = Vector2(1, 1)
+			rotation_degrees = 0
+		elif GoToObj.global_position.x < position.x && scale == Vector2.ONE:
+			scale = Vector2(1, -1)
+			rotation_degrees = 180
 
 
 func _on_bee_colony_honey_collected() -> void:
