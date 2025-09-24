198a80a01664b293714d9977d4b9326f0e475109
MrOnosa <supersnapple@gmail.com>
2025-09-23 21:15:24 -0500

    Cancelling item placement if over the hud or if there is not room on the map.

--- scenes/world.gd
+++ scenes/world.gd
@@ -8,6 +8,7 @@ const BEE_COLONY = preload("uid://cnqxl80su0co4")
 @onready var bee_keeper: BeeKeeper = $BeeKeeper
 
 
+var mouse_is_over_HUD : bool = false
 var drag_and_drop_item : Variant = null
 #const WORLD_GRASS_NOISE_TEXTURE = preload("uid://c0a00g37gmqql")
 
@@ -20,20 +21,38 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if drag_and_drop_item != null:
+	if drag_and_drop_item != null && !drag_and_drop_item.is_queued_for_deletion():
 		# https://www.reddit.com/r/godot/comments/yge1ms/comment/iucbhph/
 		drag_and_drop_item.position = drag_and_drop_item.get_parent().get_local_mouse_position()
-		if Input.is_action_just_released("left_click"):
-			var bee_colony = BEE_COLONY.instantiate() as BeeColony
-			bee_colony.position = drag_and_drop_item.position
-			bee_colony.total_bees = 8000
-			bee_colony.raw_honey_produced = bee_colony.raw_honey_needed_for_a_jar - 0.1
-			bee_colony.max_population = 20000
-			bee_colony.max_raw_honey_capacity = bee_colony.raw_honey_needed_for_a_jar * 2
-			bee_colony.honey_collected.connect(bee_keeper._on_bee_colony_honey_collected)
-			bee_colony.spawn_bee.connect(_on_bee_colony_spawn_bee)
-			add_child(bee_colony)
-			drag_and_drop_item = null
+		drag_and_drop_item.modulate = Color.WHITE
+		var any_collisions : bool = false
+		if !mouse_is_over_HUD:
+			var all_bee_colonies = get_tree().get_nodes_in_group("bee_colony") as Array[BeeColony]
+			for b in all_bee_colonies:
+				if b.get_node("InteractiveArea2D").overlaps_area(drag_and_drop_item.get_node("InteractiveArea2D")):
+					any_collisions = true
+					drag_and_drop_item.modulate = Color.INDIAN_RED
+					break
+		
+		if  Input.is_action_just_released("left_click"):
+			if !mouse_is_over_HUD:				
+				# Check if the space is free				
+				if any_collisions:
+					#todo - Sfx that goes AANT!
+					pass
+				else:
+					var bee_colony = BEE_COLONY.instantiate() as BeeColony
+					bee_colony.position = drag_and_drop_item.position
+					bee_colony.total_bees = 8000
+					bee_colony.raw_honey_produced = bee_colony.raw_honey_needed_for_a_jar - 0.1
+					bee_colony.max_population = 20000
+					bee_colony.max_raw_honey_capacity = bee_colony.raw_honey_needed_for_a_jar * 2
+					bee_colony.honey_collected.connect(bee_keeper._on_bee_colony_honey_collected)
+					bee_colony.spawn_bee.connect(_on_bee_colony_spawn_bee)
+					add_child(bee_colony)
+			
+			# Always clear this even if they are still over the hud.
+			drag_and_drop_item.queue_free()
 
 func _on_bee_colony_spawn_bee(home_hive: BeeColony) -> void:	
 	var bee : Bee = BEE.instantiate()
@@ -47,3 +66,11 @@ func _on_natural_bee_hive_button_pressed() -> void:
 	drag_and_drop_item = new_bee_colony
 	add_child(drag_and_drop_item)
 	pass # Replace with function body.
+
+
+func _HUD_on_panel_container_mouse_entered() -> void:
+	mouse_is_over_HUD = true
+
+
+func _HUD_on_panel_container_mouse_exited() -> void:
+	mouse_is_over_HUD = false
